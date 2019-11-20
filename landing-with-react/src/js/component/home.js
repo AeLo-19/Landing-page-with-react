5f6a5232911fa7612235e64cabb602012e623a43
AeLo-19 <adrianeduardo.lopezo@gmail.com>
2019-11-20 16:55:24 +0000

    arreglos y jumbotron

--- landing-with-react/src/js/component/home.js
+++ landing-with-react/src/js/component/home.js
@@ -1,10 +1,14 @@
 import React from "react";
 import { NavBar } from "./navBar.js";
+import {Jumbotron} from "./Jumbotron.js"
 
 export function Home() {
 	return (
 		<React.Fragment>
 			<NavBar />
+            <Jumbotron />
+            <Cards />
+            <Footer />
 		</React.Fragment>
 	);
 }
