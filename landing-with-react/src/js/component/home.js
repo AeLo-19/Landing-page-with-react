b3cbdb4e23fa94a389d325d30f7c70aa40f22fa6
AeLo-19 <adrianeduardo.lopezo@gmail.com>
2019-11-20 17:25:17 +0000

    prueba

--- landing-with-react/src/js/component/home.js
+++ landing-with-react/src/js/component/home.js
@@ -1,6 +1,8 @@
 import React from "react";
 import { NavBar } from "./navBar.js";
-import {Jumbotron} from "./Jumbotron.js"
+import {Jumbotron} from "./Jumbotron.js";
+import {Footer} from "./Footer.js";
+import {Cards} from "./cards.js"
 
 export function Home() {
 	return (
